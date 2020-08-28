99ceea2469f1d5be50552bacaf16385e82899bbb
James 'zofrex' Sanderson <zofrex@gmail.com>
2020-08-28 14:39:08 +0100

    Bump version to 0.1.0

--- lib/runeterra_cards/version.rb
+++ lib/runeterra_cards/version.rb
@@ -2,5 +2,5 @@
 
 module RuneterraCards
   # The version of this library
-  VERSION = '0.0.1'
+  VERSION = '0.1.0'
 end
--- test/test_runeterra_cards.rb
+++ test/test_runeterra_cards.rb
@@ -5,6 +5,6 @@ require_relative 'test_helper'
 describe RuneterraCards do
   cover 'RuneterraCards::VERSION'
   it 'returns a version' do
-    _(RuneterraCards::VERSION).must_equal '0.0.1'
+    _(RuneterraCards::VERSION).must_equal '0.1.0'
   end
 end
