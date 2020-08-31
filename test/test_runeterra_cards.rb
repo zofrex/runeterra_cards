2995c98023c17307d8c19b50bdd57d2178bfc33a
James 'zofrex' Sanderson <zofrex@gmail.com>
2020-08-31 10:17:48 +0100

    Revert "Make a single source of truth for version, even for docs"
    This reverts commit 9fd1c815c150687d49320e7c3715b2fbb41a1e86.
    
    Turns out this doesn't work on rubydoc.info

---
+++ test/test_runeterra_cards.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+
+describe RuneterraCards do
+  cover 'RuneterraCards::VERSION'
+  it 'returns a version' do
+    _(RuneterraCards::VERSION).must_equal '0.2.1'
+  end
+end
