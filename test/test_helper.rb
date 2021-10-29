4189420d84ddc7c2ebde96d604ab05201d9702a0
James 'zofrex' Sanderson <zofrex@gmail.com>
2021-10-29 09:54:52 +0100

    Fix Rubocop issues

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,13 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 
 if ENV['LOAD_MUTANT']
+  # Only load mutant if the env var is set, so we can skip loading the gem at all for normal unit test runs
   require 'mutant/minitest/coverage'
 else
+  # If we don't load mutant, we need to stub the `cover` method to avoid errors
   module CoverageStub
     def cover(expression)
+      # this method intentionally left blank
     end
   end
   Minitest::Test.extend(CoverageStub)
